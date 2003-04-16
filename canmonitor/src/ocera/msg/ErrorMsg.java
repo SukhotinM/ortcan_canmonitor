73f43e2af998bfe49e9de05fa844c322240dcf77
fanda <fanda>
2003-04-16 07:15:21 +0000

    *** empty log message ***

--- canmonitor/src/ocera/msg/ErrorMsg.java
+++ canmonitor/src/ocera/msg/ErrorMsg.java
@@ -5,14 +5,13 @@ import java.awt.*;
 
 /**
  * @author vacek
- *
  * To change this generated comment edit the template variable "typecomment":
  * Window>Preferences>Java>Templates.
  * To enable and disable the creation of type comments go to
  * Window>Preferences>Java>Code Generation.
  */
 
-public class ErrorMsg 
+public class ErrorMsg
 {
 	private Component parent = null;
 	
