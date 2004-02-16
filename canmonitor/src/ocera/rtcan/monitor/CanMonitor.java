57dc8f0f14446db66c806bf9e87ca6657615432f
fanda <fanda>
2004-02-16 11:00:15 +0000

    SYNC generation supported

--- canmonitor/src/ocera/rtcan/monitor/CanMonitor.java
+++ canmonitor/src/ocera/rtcan/monitor/CanMonitor.java
@@ -758,7 +758,6 @@ public class CanMonitor extends JFrame implements Runnable {
                 ss = StringParser.cutInt(s, 16); s = ss[1]; //index
                 index = FString.toInt(ss[0], 16);
                 ss = StringParser.cutInt(s, 16); s = ss[1]; //subindex
-        txtMsg.append("ss[0]: " + ss[0] + "\n");
                 subindex = FString.toInt(ss[0], 16);
                 // check abort
                 s = s.trim();
