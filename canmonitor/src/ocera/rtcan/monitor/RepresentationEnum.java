1bef6f346ac318617b280f6ef4fc36fbd5dac122
Kubasta Petr <kubaspet@fel.cvut.cz>
2012-06-20 12:39:55 +0200

    add: enumerator class for choosing of views

---
+++ canmonitor/src/ocera/rtcan/monitor/RepresentationEnum.java
@@ -0,0 +1,12 @@
+package ocera.rtcan.monitor;
+
+/**
+ * User: kubaspet
+ */
+public final class RepresentationEnum {
+    private RepresentationEnum(){}
+
+    public static final String [] EnumNames={"hex_raw", "bin_raw"};
+    public static final int HEX_RAW = 0;
+    public static final int BIN_RAW = 1;
+}
