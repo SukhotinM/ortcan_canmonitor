9e979d07573e22dad490fda417315f6ab029e682
Kubasta Petr <kubaspet@fel.cvut.cz>
2012-06-20 14:34:46 +0200

    add: Exception class InvalidTypeOfViewException

---
+++ canmonitor/src/ocera/rtcan/monitor/InvalidTypeOfViewException.java
@@ -0,0 +1,10 @@
+package ocera.rtcan.monitor;
+
+public class InvalidTypeOfViewException extends Exception {
+    public InvalidTypeOfViewException() {
+        super();
+    }
+    public InvalidTypeOfViewException(String a) {
+        super(a);
+    }
+}
