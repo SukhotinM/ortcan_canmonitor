040b154228745b42baf43aebeeae1d05a4e41757
Kubasta Petr <kubaspet@fel.cvut.cz>
2012-06-20 14:38:26 +0200

    add: choice to enumerator class

--- canmonitor/src/ocera/rtcan/monitor/RepresentationEnum.java
+++ canmonitor/src/ocera/rtcan/monitor/RepresentationEnum.java
@@ -6,7 +6,8 @@ package ocera.rtcan.monitor;
 public final class RepresentationEnum {
     private RepresentationEnum(){}
 
-    public static final String [] EnumNames={"hex_raw", "bin_raw"};
+    public static final String [] EnumNames={"hex_raw", "bin_raw","type_pref"};
     public static final int HEX_RAW = 0;
     public static final int BIN_RAW = 1;
+    public static final int TYPE_PREF = 2;
 }
